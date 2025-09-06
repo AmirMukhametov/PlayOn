f9bec7222b5894b84267ef696de9ecfe28d9deb5
AidarProgrammer <aidarchess27@gmail.com>
2025-09-06 18:42:17 +0300

    Добавлен header без адаптации, роутинг, страницы

---
+++ play-on/src/shared/config/routesMasks.ts
@@ -0,0 +1,32 @@
+export const routesMasks = {
+  main: {
+    mask: '/',
+    create: () => '/',
+  },
+  login: {
+    mask: '/login',
+    create: () => '/login',
+  },
+  signup: {
+    mask: '/signup',
+    create: () => '/signup',
+  },
+  movies: {
+    mask: '/movies',
+    create: () => '/movies',
+  },
+  series: {
+    mask: '/series',
+    create: () => '/series',
+  },
+  top: {
+    mask: '/top',
+    create: () => '/top',
+  },
+  profile: {
+    mask: '/profile',
+    create: () => '/profile',
+  }
+};
+
+export type RouteKeys = keyof typeof routesMasks;
